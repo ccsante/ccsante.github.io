8f3f00ce78c6b62c2fd6dead4e1628a6df3e6e32
mathuramg <mmg9291@gmail.com>
2019-05-13 17:22:56 +0530

    responsive nav

--- js/sketch.js
+++ js/sketch.js
@@ -1,145 +1,65 @@
-/*
-Frozen brush
-
-Makes use of a delaunay algorithm to create crystal-like shapes.
-I did NOT develop delaunay.js, and not sure who the author really is to give proper credit.
-
-Controls:
-	- Drag the mouse.
-    - Press any key to toggle between fill and stroke.
-
-Inspired by:
-	Makio135's sketch www.openprocessing.org/sketch/385808
-
-Author:
-  Jason Labbe
-
-Site:
-  jasonlabbe3d.com
-*/
-
-var allParticles = [];
-var maxLevel = 5;
-var useFill = false;
-
-var data = [];
-
-
-// Moves to a random direction and comes to a stop.
-// Spawns other particles within its lifetime.
-function Particle(x, y, level) {
-  this.level = level;
-  this.life = 0;
-
-  this.pos = new p5.Vector(x, y);
-  this.vel = p5.Vector.random2D();
-  this.vel.mult(map(this.level, 0, maxLevel, 5, 2));
-
-  this.move = function() {
-    this.life++;
-
-    // Add friction.
-    this.vel.mult(0.9);
-
-    this.pos.add(this.vel);
-
-    // Spawn a new particle if conditions are met.
-    if (this.life % 10 == 0) {
-      if (this.level > 0) {
-        this.level -= 1;
-        let newParticle = new Particle(this.pos.x, this.pos.y, this.level-1);
-        allParticles.push(newParticle);
-      }
-    }
-  }
-}
+var colourx = [];
+var coloury = [];
+var colourz = [];
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-
-  background(255);
-  // colorMode(HSB, 360);
-
-  textAlign(CENTER);
+  /* Use for static colours
+  COMMENT HERE IF YOU WANT DYNAMIC COLOURS*/
+  //setColours();
 
 }
 
-
-function draw() {
-  // Create fade effect.
-  noStroke();
-	colorMode(RGB);
-  fill(255, 30);
-	colorMode(HSB, 360);
-  rect(0, 0, width, height);
-
-  // Move and spawn particles.
-  // Remove any that is below the velocity threshold.
-  for (var i = allParticles.length-1; i > -1; i--) {
-    allParticles[i].move();
-
-    if (allParticles[i].vel.mag() < 0.01) {
-      allParticles.splice(i, 1);
-    }
-  }
-
-  if (allParticles.length > 0) {
-    // Run script to get points to create triangles with.
-    data = Delaunay.triangulate(allParticles.map(function(pt) {
-      return [pt.pos.x, pt.pos.y];
-    }));
-
-    strokeWeight(0.1);
-
-    // Display triangles individually.
-    for (var i = 0; i < data.length; i += 3) {
-      // Collect particles that make this triangle.
-      let p1 = allParticles[data[i]];
-      let p2 = allParticles[data[i+1]];
-      let p3 = allParticles[data[i+2]];
-
-      // Don't draw triangle if its area is too big.
-      let distThresh = 75;
-
-      if (dist(p1.pos.x, p1.pos.y, p2.pos.x, p2.pos.y) > distThresh) {
-        continue;
-      }
-
-      if (dist(p2.pos.x, p2.pos.y, p3.pos.x, p3.pos.y) > distThresh) {
-        continue;
-      }
-
-      if (dist(p1.pos.x, p1.pos.y, p3.pos.x, p3.pos.y) > distThresh) {
-        continue;
-      }
-
-      // Base its hue by the particle's life.
-      if (useFill) {
-        noStroke();
-        fill(165+p1.life*1.5);
-      } else {
-        noFill();
-        stroke(p1.life*7);
-      }
-
-      triangle(p1.pos.x, p1.pos.y,
-               p2.pos.x, p2.pos.y,
-               p3.pos.x, p3.pos.y);
-    }
+function getPoints(xc, yc) {
+  for (let i = 0; i < count; i++) {
+    xarr[i] = xc + (rad * sin((2 * (22 / 7) * i) / count));
+    yarr[i] = yc + (rad * cos((2 * (22 / 7) * i) / count));
+    point(xarr[i], yarr[i]);
   }
 }
 
+function setColours() {
+  for (let i = 0; i < 50; i++) {
 
-function mouseMoved() {
-  allParticles.push(new Particle(mouseX, mouseY, maxLevel));
+    colourx[i] = random() * 255;
+    coloury[i] = random() * 255;
+    colourz[i] = random() * 255;
+
+  }
 }
 
+function draw() 
 
-function touchMoved() {
-  allParticles.push(new Particle(mouseX, mouseY, maxLevel));
+{
+  stroke(128);
+  strokeWeight(0.5)
+  lines(50, height/2, 14, 200);
+  lines(150,80, 7, 50);
+  lines(width - 100, height*0.8, 11 , 150);
+  lines(width +200, height*0.1, 21 , 350);
+
+
 }
 
-function keyPressed() {
-  useFill = ! useFill;
+function lines(xc, yc, count, rad) {
+
+  let xarr = [];
+  let yarr = [];
+
+
+  for (var i = 0; i < count; i++) {
+    xarr[i] = xc + (rad * sin((2 * (22 / 7) * i) / count));
+    yarr[i] = yc + (rad * cos((2 * (22 / 7) * i) / count));
+    point(xarr[i], yarr[i]);
+  }
+
+  for (var i = 0; i < count; i++) {
+    for (let j = 0; j < count; j++) {
+      if (((i - j) != 1) && ((j - i) != 1)) {
+        //stroke(255 / 16 * j, 255 / 256 * i * j, 255 / 16 * i);
+        line(xarr[i], yarr[i], xarr[j], yarr[j]);
+      }
+    }
+  }
 }
